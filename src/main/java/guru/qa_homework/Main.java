89bbc0709d845a2487c682e7c774af6b4e0e5cd3
BlankFlyleaf <83223205+BlankFlyleaf@users.noreply.github.com>
2025-02-04 09:15:33 +0300

    Update Main.java

--- src/main/java/guru/qa_homework/Main.java
+++ src/main/java/guru/qa_homework/Main.java
@@ -1,9 +1,9 @@
 package guru.qa_homework;
 
 public class Main {
-    public static String msg = "Hello, QaGuru! I'm doing my part!"
+    public static String msg = "Hello, QaGuru! I'm doing my part!";
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(msg);
     }
 }
