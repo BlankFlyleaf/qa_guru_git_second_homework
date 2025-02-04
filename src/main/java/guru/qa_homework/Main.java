042ff9315a1a9599027f88286001a8cac473af5f
BlankFlyleaf <kriechenrosenrot@gmail.com>
2025-02-04 09:14:45 +0300

    New Changes

--- src/main/java/guru/qa_homework/Main.java
+++ src/main/java/guru/qa_homework/Main.java
@@ -2,8 +2,12 @@ package guru.qa_homework;
 
 public class Main {
     public static String msg = "Hello, QaGuru! I'm doing my part!"
-    
+
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(getMessage ());
     }
+}
+
+private static String getMessage () {
+    return "msg";
 }
